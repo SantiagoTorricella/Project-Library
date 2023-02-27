4c6084f2fae8143451d1ec288ff4dda31acb4c06
Santiago Torricella <torrisantiago@gmail.com>
2023-02-27 12:46:20 -0300

    logic js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Nodes references //
 const addBook = document.querySelector(".add-book");
 const form = document.querySelector("#myForm");
+const navBar = document.querySelector(".nav-bar");
+const sideBar = document.querySelector(".side-bar");
+const content = document.querySelector(".content");
+const sumbitButton = document.querySelector(".sumbit-button");
 
 // Variables //
 let myLibrary = [];
@@ -8,10 +12,10 @@ let myLibrary = [];
 // Classes and constructors //
 class Book {
   constructor(
-    title = "Unknown",
-    author = "Unknown",
+    title = "aguante instituto de cordoba",
+    author = "el decano",
     pages = "0",
-    read = "Unknown"
+    read = "caco virgo"
   ) {
     this.title = title;
     this.author = author;
@@ -21,10 +25,39 @@ class Book {
 }
 
 // Functions //
-function addBookToLibrary(book) {}
 
 function openForm() {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
-  } else form.classList.add("hidden");
+    navBar.classList.add("blurredElement");
+    sideBar.classList.add("blurredElement");
+    content.classList.add("blurredElement");
+  } else {
+    form.classList.add("hidden");
+    navBar.classList.remove("blurredElement");
+    sideBar.classList.remove("blurredElement");
+    content.classList.remove("blurredElement");
+  }
 }
+
+function getBook() {
+  const title = document.getElementById("bookName").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("readOrNotRead").value;
+
+  return new Book(title, author, pages, read);
+}
+
+sumbitButton.addEventListener("click", () => {
+  addBookToLibrary();
+});
+
+function addBookToLibrary() {
+  let newBook = getBook();
+  myLibrary.push(newBook);
+}
+
+myLibrary.forEach((book) => {
+  content.appendChild(book);
+});
